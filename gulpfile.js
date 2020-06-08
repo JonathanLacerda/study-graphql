d6f1be2df1f1d7eae0de6a5f12b0d634cb02d829
jlacerda <jlacerda@itelios.com>
2020-06-08 10:39:49 -0300

    feat: gulp tasks and npm script

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,30 @@ const ts = require('gulp-typescript')
 
 const tsApp = ts.createProject('tsconfig.json')
 
-gulp.task('scripts', () => {
+gulp.task('scripts', ['static'], () => {
     const tsResult = tsApp.src()
         .pipe(tsApp())
 
     return tsResult.js
         .pipe(gulp.dest('dist'))
 })
+
+gulp.task('static', ['clean'], () => {
+    return gulp
+        .src(['src/**/*.js'])
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('clean', () => {
+    return gulp
+        .src('dist')
+        .pipe(clean())
+})
+
+gulp.task('build', ['scripts'])
+
+gulp.task('watch', ['build'], () => {
+    return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build'])
+})
+
+gulp.task('default', ['watch'])
