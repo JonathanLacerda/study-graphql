3a4c9d8ea107ad55a90774f77beee2be65aec7ee
jlacerda <jlacerda@itelios.com>
2020-06-08 11:16:21 -0300

    feat: install and configure express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
-console.log('Initial')
+import * as express from 'express'
 
-var teste: string = 'teste 03'
 
-console.log(teste)
+class App {
+    public express : express.Application
+
+    /**
+     *  Inicializador do express
+     */
+    constructor(){
+        this.express = express()
+        this.middleware()
+    }
+
+    /**
+     *  Método privado
+     *  Setando rota
+     */
+    private middleware(): void {
+        this.express.use('/', (req: express.Request, res: express.Response, next:express.NextFunction) => {
+            res.send({
+                hello: 'Teste'
+            })
+        })
+    }
+}
+
+export default new App().express
